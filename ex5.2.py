75e0a0826a71f4ca84c6efca3fb390414729a9dd
felixbako <felixbako@gmail.com>
2016-07-18 12:51:45 -0400

    upload ex5.2

---
+++ ex5.2.py
@@ -0,0 +1,18 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Lets talk about {}".format(my_name)
+print "He's {} inches tall".format(my_height)
+print "He's {} pounds heavy.".format(my_weight)
+print "He's got {0} eyes and {0} hair.".format(my_eyes, my_hair)
+print "His teeth are usually {} depending on the coffe.".format(my_teeth)
+
+# This is a tricky line , try to gt it exactly right
+
+print " If I add {} , {} , and {} I get {}.".format(my_age, my_height, my_weight, my_age + my_height + my_weight)
+
