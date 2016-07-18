f20ac49f33d1af54a11460e30afdb32ff8106f92
felixbako <felixbako@gmail.com>
2016-07-18 12:37:02 -0400

    upload ex5

--- ex5.py
+++ ex5.py
@@ -6,11 +6,11 @@ my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
-print "Lets talk about %s." % my_name
+print "Lets talk about {}".format(my_name)
 print "He's %d inches tall" % my_height
 print "He's %d pounds heavy"% my_weight
 print "Actually thats not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "He's got {} eyes and {} hair.".format(my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." %my_teeth
 
 # this line is tricky, try to get it exactly right
