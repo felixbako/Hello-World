197838e96b321b917acf81c34bbcb7b9651864a6
felixbako <felixbako@gmail.com>
2016-07-19 09:38:53 -0400

    upload ex6.2

---
+++ ex6.2.py
@@ -0,0 +1,21 @@
+x = "There are {} types of people.".format(10)
+binary = 'binary'
+do_not = "don't"
+y = "Those who know {} and those who {}.".format(binary, do_not)
+
+print x
+print y
+
+print "I said: {}." .format(x)
+print "I also said: '{}'." .format(y)
+
+hilarious = False
+joke_evaluation = "Isnt that joke so funny?! {}".format(hilarious)
+
+print joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with s right side."
+
+print w + e
+
